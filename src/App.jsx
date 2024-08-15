47044622b5d694cd73bc4dadbb41b54b9e19aed7
NThann22 <joki2206@gmail.com>
2024-08-15 19:06:28 +0700

    feat: :sparkles: FIrst Boiler Plate

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import { Button } from "./components/ui/button"
+
+function App() {
+
+  return (
+    <div className=" flex justify-between items-center">
+      <h1 className=" text-black text-xl font-extrabold text-center ">Test 123</h1>
+      <Button className="font-lato">Hire Me</Button>
+      <h1 className=" text-black text-xl font-extrabold font-lato text-center ">Test 123</h1>
+    </div>
+  )
+}
+
+export default App
